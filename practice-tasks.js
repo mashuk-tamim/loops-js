95e244367fb4c2baabe2e5e739e64ff749b12bcf
mashuk tamim <tamimmashuk328@gmail.com>
2023-07-31 13:53:36 +0600

    simple js code

--- practice-tasks.js
+++ practice-tasks.js
@@ -14,9 +14,33 @@
 // console.log(avgMark.toFixed(2));
 
 // task 3
-var string1='I am going to be';
-var string2='an awesome web developer';
+// var string1='I am going to be';
+// var string2='an awesome web developer';
 
-console.log(string1 + ' ' + string2); 
-var result= string1.concat(' ', string2); //using concat() function
-console.log(result);
+// console.log(string1 + ' ' + string2); 
+// var result= string1.concat(' ', string2); //using concat() function
+// console.log(result);
+
+// task 4
+// var bookPrice= [138, 234, 675, 632, 345, 23, 742, 89, 99];
+// for(var i=0; i<bookPrice.length; i++){
+//     if(bookPrice[i]>=200)
+//         console.log(bookPrice[i]);
+// }
+
+// task 4: alternative
+// var bookPrice= [138, 234, 675, 632, 345, 23, 742, 89, 99];
+// for(var i=0; i<bookPrice.length; i++){
+//     if(bookPrice[i]<=200)
+//         continue;
+//     else
+//     console.log(bookPrice[i]);
+// }
+
+// task 5
+var friendName=['John', 'James', 'Jane', 'Ratul']
+for(var i=0; i<friendName.length; i++){
+    if(friendName[i].length%2==0){
+        console.log('Even number of letter!')
+    }
+}
